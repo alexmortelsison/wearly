99ea05a539968eaffb8dbb0ecae1a71f9436ac17
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-03-01 18:29:03 +0900

    updated Hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,22 +1,33 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Hero() {
   return (
-    <div className="relative min-h-screen w-full grid place-items-center text-center">
+    <div className="grid place-items-center min-h-screen">
       <video
-        autoPlay
-        loop
-        muted
         src="/herovid.mp4"
-        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
-      ></video>
-      <div className="max-w-7xl px-6">
-        <h1 className="text-white text-6xl font-extrabold leading-tight">
-          Wear the look. Own the moment.
+        loop
+        autoPlay={true}
+        muted
+        className="-z-10 absolute"
+      />
+      <div className="max-w-7xl mx-auto text-center justify-center">
+        <h1 className=" text-6xl font-extrabold text-white">
+          Wear the look, own the moment,
         </h1>
-        <p className="text-lg leading-snug mt-2 text-muted-foreground">
+        <p className="text-gray-100 mt-4 text-2xl font-semibold">
           Redefining streetwear with bold designs and fearless confidence.
           Crafted for trendsetters, inspired by the streets, and made for those
           who dare to stand out. Wear the look, own the moment.
         </p>
+        <Link href={"/collection"}>
+          <Button
+            variant={"outline"}
+            className="mt-16 text-lg px-8 py-2 text-white bg-zinc-900 hover:bg-zinc-800 hover:text-gray-300"
+          >
+            See more
+          </Button>
+        </Link>
       </div>
     </div>
   );
