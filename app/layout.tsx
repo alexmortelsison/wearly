b2d8db8543086eac0b41c6ab9f22a031e9568c6e
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-03-02 19:44:18 +0900

    updated Products Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SessionWrapper from "./components/SessionWrapper";
 import Header from "@/components/Header/Header";
 
 const geistSans = Geist({
@@ -29,15 +28,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionWrapper>
-          <div className="min-h-screen flex flex-col">
-            <Header />
-            {children}
-            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-              <p>Copyright&copy; Wearly 2025 - All rights reserved.</p>
-            </footer>
-          </div>
-        </SessionWrapper>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          {children}
+          <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <p>Copyright&copy; Wearly 2025 - All rights reserved.</p>
+          </footer>
+        </div>
       </body>
     </html>
   );
